48929da2ce4f3e477a3a35609667244f959c9076
imehc <richohealthy@hotmail.com>
2023-09-15 10:17:02 +0800

    feat: init

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,47 @@ import Icons from "unplugin-icons/vite";
 import IconsResolver from "unplugin-icons/resolver";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
+// import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
+      // 这里除了引入 vue 以外还可以引入pinia、vue-router、vueuse等，
+      // 甚至你还可以使用自定义的配置规则，见 https://github.com/antfu/unplugin-auto-import#configuration
       imports: ["vue"],
+      // 第三方组件库的解析器
+      // resolvers: [ElementPlusResolver()],
       resolvers: [
         IconsResolver({
           prefix: "Icon",
         }),
       ],
+      dts: "./src/auto-imports.d.ts",
+      // Generate corresponding .eslintrc-auto-import.json file.
+      // eslint globals Docs - https://eslint.org/docs/user-guide/configuring/language-options#specifying-globals
       eslintrc: {
-        enabled: true,
+        enabled: true, // Default `false`
+        filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
       },
     }),
     Components({
-      resolvers: [IconsResolver()],
+      // dirs 指定组件所在位置，默认为 src/components
+      // 可以让我们使用自己定义组件的时候免去 import 的麻烦
+      dirs: ["src/components/"],
+      // 配置需要将哪些后缀类型的文件进行自动按需引入
+      extensions: ["vue", "md"],
+      // 解析的 UI 组件库，这里以 Element Plus 和 Ant Design Vue 为例
+      // resolvers: [ElementPlusResolver(), AntDesignVueResolver()],
+      resolvers: [
+        IconsResolver({
+          // enabledCollections: ['ep'], //@iconify-json/ep 是 Element Plus 的图标库，所以 IconsResolver 配置了 enabledCollections: ['ep']
+          // enabledCollections: ["ep"],
+        }),
+      ],
+      dts: "./src/components.d.ts",
     }),
     Icons({
       // https://icones.netlify.app/
